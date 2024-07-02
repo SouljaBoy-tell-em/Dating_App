7ecd998d4c2516be0db7f4914243bcebde9c975b
hellprogmaker <60592559+hellprogmaker@users.noreply.github.com>
2024-07-02 13:18:03 +0300

    updated body of general updating request

--- server/src/main/java/com/project/requests/admin/SecondaryInfoUserUpdateRequest.java
+++ server/src/main/java/com/project/requests/admin/SecondaryInfoUserUpdateRequest.java
@@ -1,6 +1,7 @@
 package com.project.requests.admin;
 
 
+import com.project.user_config.main.ZodiacSign;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import java.time.LocalDate;
@@ -11,6 +12,8 @@ import java.time.LocalDate;
 public class SecondaryInfoUserUpdateRequest {
     private String firstname;
     private String lastname;
+    private String city;
+    private ZodiacSign zodiacSign;
     private LocalDate birthday;
     private boolean isPrivate;
 }
